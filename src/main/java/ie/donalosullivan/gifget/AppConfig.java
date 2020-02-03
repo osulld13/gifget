ca48dc86f48e0cdf5a569c5c6bcb626a890ff3b3
osulld13 <osulld13@tcd.ie>
2020-02-03 23:19:44 +0000

    Initial commit: Gradle Config, App Config.

---
+++ src/main/java/ie/donalosullivan/gifget/AppConfig.java
@@ -0,0 +1,12 @@
+package ie.donalosullivan.gifget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppConfig {
+
+    public static void main (String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
