ba26e94259a7ee0765e26ff2933c7b9646446091
osulld13 <osulld13@tcd.ie>
2020-02-04 22:10:49 +0000

    Updated controller and assets

--- src/main/java/ie/donalosullivan/gifget/controller/GifController.java
+++ src/main/java/ie/donalosullivan/gifget/controller/GifController.java
@@ -1,4 +1,21 @@
 package ie.donalosullivan.gifget.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class GifController {
+
+    @RequestMapping(value = "/")
+    public String listGifs() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/gif")
+    @ResponseBody
+    public String getGif() {
+        return "A GIF!!";
+    }
+
 }
